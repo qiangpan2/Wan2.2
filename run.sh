fc67aa51cf8718eb3a8ba195224704c652b57027
qiangpan <qiangpan@amd.com>
2025-11-11 08:04:27 +0000

    integrate miopen

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ uv sync --python /opt/conda/envs/py_3.12/bin/python
 #rccl check
 export NCCL_DEBUG=INFO
 python -c "import torch; print(torch.distributed.is_nccl_available())"
-LD_LIBRARY_PATH=/workspace/repo/rccl/build/debug/rccl_deps/lib:$LD_LIBRARY_PATH torchrun --nproc_per_node=4 test_nccl.py >  test_nccl.log 2>&1
+LD_LIBRARY_PATH=/workspace/repo/rccl/build/debug/rccl_deps/lib:$LD_LIBRARY_PATH torchrun --nproc_per_node=8 test_nccl.py >  test_nccl.log 2>&1
 
 
 find -name "ring_flashinfer_attn.py"
@@ -34,8 +34,9 @@ rocprof-sys-avail -G rocprof-sys.cfg
 uv run  python test_profile.py > test_profile.log 2>&1
 
 # 8 cards I2V
-
-LD_LIBRARY_PATH=/workspace/repo/rccl/build/debug/rccl_deps/lib:$LD_LIBRARY_PATH  uv run torchrun --nproc_per_node=4 generate.py --offload_model True --task i2v-A14B --size 1280*720 --ckpt_dir ./Wan2.2-I2V-A14B --image examples/i2v_input.JPG --dit_fsdp --t5_fsdp --ulysses_size 4 --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > output.log 2>&1
+LD_LIBRARY_PATH=/workspace/repo/rocm-libraries/projects/miopen/build/lib:\
+    /workspace/repo/rccl/build/debug/rccl_deps/lib:$LD_LIBRARY_PATH \
+    uv run torchrun --nproc_per_node=4 generate.py --offload_model True --task i2v-A14B --size 1280*720 --ckpt_dir ./Wan2.2-I2V-A14B --image examples/i2v_input.JPG --dit_fsdp --t5_fsdp --ulysses_size 4 --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > output.log 2>&1
 
 #T2V
-uv run python generate.py --task ti2v-5B --size 1280*704 --ckpt_dir ./Wan2.2-TI2V-5B --offload_model True --convert_model_dtype --t5_cpu --image examples/i2v_input.JPG --frame_num 3  --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > Wan2.2-TI2V-5B_output.log 2>&1
+LD_LIBRARY_PATH=/workspace/repo/rocm-libraries/projects/miopen/build/lib:$LD_LIBRARY_PATH uv run python generate.py --task ti2v-5B --size 1280*704 --ckpt_dir ./Wan2.2-TI2V-5B --offload_model True --convert_model_dtype --t5_cpu --image examples/i2v_input.JPG --frame_num 3  --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > Wan2.2-TI2V-5B_output.log 2>&1
