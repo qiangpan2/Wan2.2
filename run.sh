0f964db00bd54bf2cd92f3803e5d37a058e30713
qiangpan <qiangpan@amd.com>
2025-12-03 09:14:56 +0000

    refined script

--- run.sh
+++ run.sh
@@ -56,16 +56,18 @@ LD_LIBRARY_PATH=${DEPS_PREFIX}/lib:$LD_LIBRARY_PATH \
     MIOpenDriver convbfp16 -n 1 -c 3 --in_d 3 -H 1106 -W 834 -k 96 --fil_d 3 -y 3 -x 3 --pad_d 0 -p 0 -q 0 --conv_stride_d 1 -u 1 -v 1 --dilation_d 1 -l 1 -j 1 --spatial_dim 3 --in_layout NDHWC --fil_layout NDHWC --out_layout NDHWC -m conv -g 1 -F 1 -t 1 > failure.log 2>&1
 
 # ================================ 8 cards I2V ================================ 
-LD_LIBRARY_PATH=${DEPS_PREFIX}/lib:$LD_LIBRARY_PATH \
-    PATH=${DEPS_PREFIX}/bin:$PATH \
-    uv run torchrun --nproc_per_node=8 generate.py --offload_model True --task i2v-A14B --size 1280*720 --ckpt_dir ./Wan2.2-I2V-A14B --image examples/i2v_input.JPG --dit_fsdp --t5_fsdp --ulysses_size 8 --frame_num 1 --sample_step 1 --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > output.log 2>&1
-
-#T2V
-export LD_LIBRARY_PATH=/workspace/repo/rocm-libraries/projects/miopen/build/lib:$LD_LIBRARY_PATH
+export DEPS_PREFIX="${HOME}/miopen-deps"
 export MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_CHANNEL_LAST_FWD_WMMAOPS=1
+    #export MIOPEN_DEBUG_CONV_DIRECT_NAIVE_CONV_FWD=0 #avoid native evaluation time
 export MIOPEN_ENABLE_LOGGING=1
 export MIOPEN_ENABLE_LOGGING_CMD=1
 export MIOPEN_LOG_LEVEL=7
+LD_LIBRARY_PATH=${DEPS_PREFIX}/lib:$LD_LIBRARY_PATH \
+    PATH=${DEPS_PREFIX}/bin:$PATH \
+    uv run torchrun --nproc_per_node=8 generate.py --offload_model True --task i2v-A14B --size 1280*720 --ckpt_dir ./Wan2.2-I2V-A14B --image examples/i2v_input.JPG --dit_fsdp --t5_fsdp --ulysses_size 8 --frame_num 81 --sample_step 1 --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside." > output.log 2>&1
+
+#T2V
+export LD_LIBRARY_PATH=/workspace/repo/rocm-libraries/projects/miopen/build/lib:$LD_LIBRARY_PATH
 
 #I2V 8 cards 
 LD_LIBRARY_PATH=${DEPS_PREFIX}/lib:$LD_LIBRARY_PATH \
